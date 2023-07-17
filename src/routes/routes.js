7417b276fb7de4ff3a4a76e71ac7709256df11f0
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2023-07-17 13:03:53 +0100

    dashboard fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,20 +7,24 @@ import DashBoardLayout from "../components/Backbone";
 
 const AllPages = () => (
   <BrowserRouter>
-  <Routes>
-  <Route exact path="/" element={<Login />}></Route>
-  </Routes>
-  <DashBoardLayout>
- <Routes>
-      <Route exact path="/dashboard" element={<Overview />}></Route>
-      <Route exact path="/transactions" element={<h1>transactions</h1>}></Route>
-      <Route exact path="/crypto" element={<h1>crypto</h1>}></Route>
-      <Route exact path="/bills" element={<h1>bills</h1>}></Route>
-      <Route exact path="/wallets" element={<h1>wallets</h1>}></Route>
-      <Route exact path="/card" element={<h1>cards</h1>}></Route>
-      <Route exact path="/customers" element={<h1>customers</h1>}></Route>
-      <Route exact path="/settings" element={<h1>settings</h1>}></Route>
-      {/* <Route
+    <Routes>
+      <Route exact path="/" element={<Login />}></Route>
+    </Routes>
+    <DashBoardLayout>
+      <Routes>
+        <Route exact path="/dashboard" element={<Overview />}></Route>
+        <Route
+          exact
+          path="/transactions"
+          element={<h1>transactions</h1>}
+        ></Route>
+        <Route exact path="/crypto" element={<h1>crypto</h1>}></Route>
+        <Route exact path="/bills" element={<h1>bills</h1>}></Route>
+        <Route exact path="/wallets" element={<h1>wallets</h1>}></Route>
+        <Route exact path="/card" element={<h1>cards</h1>}></Route>
+        <Route exact path="/customers" element={<h1>customers</h1>}></Route>
+        <Route exact path="/settings" element={<h1>settings</h1>}></Route>
+        {/* <Route
         path="/search"
         element={
           <PrivateRoute>
@@ -28,9 +32,8 @@ const AllPages = () => (
           </PrivateRoute>
         }
       /> */}
-    </Routes>
-  </DashBoardLayout>
-   
+      </Routes>
+    </DashBoardLayout>
   </BrowserRouter>
 );
 
