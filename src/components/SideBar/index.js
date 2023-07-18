d23520c40ef6dad12b3054725ef05a8710ab2859
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2023-07-18 20:11:44 +0100

    fix side bar issue

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 import { useNavigate } from "react-router-dom";
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { ReactComponent as DashboardIcon } from "../../assets/images/dashboardIcon.svg";
+import { ReactComponent as TransactionsIcon } from "../../assets/images/transactionsIcon.svg";
+import { ReactComponent as WalletIcon } from "../../assets/images/walletIcon.svg";
+import { ReactComponent as CardIcon } from "../../assets/images/cardsIcon.svg";
+import { ReactComponent as CustomerIcon } from "../../assets/images/customersIcon.svg";
+import { ReactComponent as SettingsIcon } from "../../assets/images/settingsIcon.svg";
+import logo from "../../assets/images/x-pad-logo.svg";
 
 const { Sider } = Layout;
 
@@ -19,27 +20,27 @@ function getItem(label, key, icon, children) {
   };
 }
 const items = [
-  getItem("Dashboard", "/dashboard", <PieChartOutlined />),
-  getItem("Transactions", "sub1", <UserOutlined />, [
+  getItem("Dashboard", "/dashboard", <DashboardIcon />),
+  getItem("Transactions", "trans", <TransactionsIcon />, [
     getItem("Fiat Transactions", "/transactions"),
     getItem("Crypto Transactions", "/crypto"),
     getItem("Bill Payments", "/bills"),
   ]),
-  getItem("Wallets", "/wallets", <PieChartOutlined />),
-  getItem("Virtual Cards", "/cards", <DesktopOutlined />),
+  getItem("Wallets", "/wallets", <WalletIcon />),
+  getItem("Virtual Cards", "/cards", <CardIcon />),
 
-  getItem("Customers", "/customers", <FileOutlined />),
-  getItem("Settings", "/settings", <FileOutlined />),
+  getItem("Customers", "/customers", <CustomerIcon />),
+  getItem("Settings", "/settings", <SettingsIcon />),
 ];
 
 export default function SideBar() {
   const navigate = useNavigate();
   return (
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-    >
-      <div className="demo-logo-vertical" />
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div className="pl-7 mt-5 mb-7">
+        <img src={logo} alt="xpad logo" className="side-logo" />
+      </div>
+
       <Menu
         theme="dark"
         defaultSelectedKeys={[window.location.pathname]}
@@ -47,6 +48,7 @@ export default function SideBar() {
         onClick={({ key }) => {
           navigate(key);
         }}
+        defaultOpenKeys={['transactions']}
         items={items}
       />
     </Sider>
