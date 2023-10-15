0bc9ff0b43d4e223dd5b171bd0face9c37b536f1
chimeSomto <110493530+chimejuliet@users.noreply.github.com>
2023-10-15 19:02:26 +0100

    added date filter for dashboard chart

--- src/components/chart/ChartLabels.js
+++ src/components/chart/ChartLabels.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ChartLabels = ({ name, bgColor, number = 0 }) => {
+const ChartLabels = ({ name, bgColor, number }) => {
   return (
     <div className='flex items-center mb-3'>
       <div className={`w-2 h-2 ${name === 'Deposits' ? 'bg-deposit' : name === 'Swaps' ? 'bg-swaps' : name === 'Withdrawals' ? 'bg-withdraw' : name === 'Transfers' ? 'bg-transfer' : bgColor} mr-2 rounded-full`} />
