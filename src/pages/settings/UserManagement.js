30f696354297dd8036c6203a38c123c64a766460
chimeSomto <110493530+chimejuliet@users.noreply.github.com>
2023-11-28 14:21:17 +0100

    invite user

--- src/pages/settings/UserManagement.js
+++ src/pages/settings/UserManagement.js
@@ -1,17 +1,15 @@
 import { Dropdown } from 'antd'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SlOptionsVertical } from 'react-icons/sl'
 import CustomButton from '../../components/fields/CustomButton'
 import { color } from '../../assets/color'
 import CustomTable from '../../components/table/CustomTable'
 import { useDispatch, useSelector } from 'react-redux'
-// import { queryAllUser } from '../../services/slices/user/allUsers';
 import {
     getAdminsSelector,
     queryAdmins,
 } from '../../services/slices/admin/fetchAdmins'
-// import { queryResendInviteAdmin } from '../../services/slices/invite/resendInvite'
-// import { useErrorTimeout } from '../../hooks/useTimeout'
+import { queryResendInviteAdmin } from '../../services/slices/invite/resendInvite'
 
 const SettingActions = ({ text, color, ...props }) => {
     return (
@@ -26,10 +24,10 @@ const SettingActions = ({ text, color, ...props }) => {
     )
 }
 
-const UserManagement = ({ setAddUser }) => {
+const UserManagement = ({ setAddUser, setMessage, setStatus }) => {
     const dispatch = useDispatch()
 
-    //const [message, setMessage, status, setStatus] = useErrorTimeout()
+    const [reSendInviteLoading, setResendInviteLoading] = useState(false)
 
     const { admins, loading } = useSelector(getAdminsSelector)
 
@@ -46,8 +44,17 @@ const UserManagement = ({ setAddUser }) => {
 
     const onHandleResendInvite = async (data) => {
         try {
-            //const res = await dispatch(queryResendInviteAdmin(data))
-        } catch (e) {}
+            setResendInviteLoading(true)
+            const res = await dispatch(queryResendInviteAdmin(data)).unwrap()
+            setResendInviteLoading(false)
+            setStatus(res?.status)
+            setMessage(res?.message)
+        } catch (e) {
+            setStatus('error')
+            setMessage(e?.errorMessage)
+        } finally {
+            setResendInviteLoading(false)
+        }
     }
 
     useEffect(() => {
@@ -130,11 +137,18 @@ const UserManagement = ({ setAddUser }) => {
                                         ) : (
                                             <div className="flex flex-col">
                                                 <SettingActions
-                                                    text="Resend Invite"
+                                                    text={
+                                                        reSendInviteLoading
+                                                            ? 'Resending...'
+                                                            : 'Resend Invite'
+                                                    }
                                                     onClick={() =>
                                                         onHandleResendInvite({
                                                             email: record?.email,
                                                         })
+                                                    }
+                                                    disable={
+                                                        reSendInviteLoading
                                                     }
                                                 />
                                                 <SettingActions text="Send Password Reset" />
@@ -196,3 +210,202 @@ const UserManagement = ({ setAddUser }) => {
 }
 
 export default UserManagement
+
+// import { Dropdown } from 'antd'
+// import React, { useEffect } from 'react'
+// import { SlOptionsVertical } from 'react-icons/sl'
+// import CustomButton from '../../components/fields/CustomButton'
+// import { color } from '../../assets/color'
+// import CustomTable from '../../components/table/CustomTable'
+// import { useDispatch, useSelector } from 'react-redux'
+// // import { queryAllUser } from '../../services/slices/user/allUsers';
+// import {
+//     getAdminsSelector,
+//     queryAdmins,
+// } from '../../services/slices/admin/fetchAdmins'
+// // import { queryResendInviteAdmin } from '../../services/slices/invite/resendInvite'
+// // import { useErrorTimeout } from '../../hooks/useTimeout'
+
+// const SettingActions = ({ text, color, ...props }) => {
+//     return (
+//         <span
+//             className={`${
+//                 color || 'text-mainColor'
+//             } text-[12px] font-medium cursor my-1 cursor-pointer`}
+//             {...props}
+//         >
+//             {text}
+//         </span>
+//     )
+// }
+
+// const UserManagement = ({ setAddUser }) => {
+//     const dispatch = useDispatch()
+
+//     //const [message, setMessage, status, setStatus] = useErrorTimeout()
+
+//     const { admins, loading } = useSelector(getAdminsSelector)
+
+//     const onAddUser = () => {
+//         setAddUser(true)
+//     }
+
+//     const onHandleStatusChange = (e) => {
+//         alert(e)
+//     }
+//     const onHandleRoleChange = (e) => {
+//         alert(e)
+//     }
+
+//     const onHandleResendInvite = async (data) => {
+//         try {
+//             //const res = await dispatch(queryResendInviteAdmin(data))
+//         } catch (e) {}
+//     }
+
+//     useEffect(() => {
+//         dispatch(queryAdmins())
+//     }, [dispatch])
+
+//     const columns = [
+//         {
+//             title: 'No',
+//             dataIndex: '',
+//             key: '',
+//             render: (_, title, index) => {
+//                 return <p>{index + 1}.</p>
+//             },
+//         },
+//         {
+//             title: 'User Name',
+//             dataIndex: 'name',
+//             key: 'name',
+//             render: (_, title, index) => {
+//                 return <p>{title.lastName + ' ' + title.firstName}.</p>
+//             },
+//         },
+//         {
+//             title: 'Email Address',
+//             dataIndex: 'email',
+//             key: 'email',
+//         },
+//         {
+//             title: 'Role',
+//             dataIndex: '',
+//             key: '',
+//             render: (_, title, index) => {
+//                 return <p>{(title.roles || [])[0]?.name}</p>
+//             },
+//         },
+//         // {
+//         //     title: 'Last Login',
+//         //     dataIndex: 'date',
+//         //     key: 'date',
+//         // },
+//         // {
+//         //     title: 'Status',
+//         //     dataIndex: 'status',
+//         //     key: 'status',
+//         //     render: (text) => {
+//         //         return (
+//         //             <p
+//         //                 className={`${
+//         //                     text === 'active'
+//         //                         ? 'text-statusGreen'
+//         //                         : text === 'pending'
+//         //                         ? 'text-statusPending'
+//         //                         : null
+//         //                 }`}
+//         //             >
+//         //                 {text}
+//         //             </p>
+//         //         )
+//         //     },
+//         // },
+//         {
+//             title: ' ',
+//             dataIndex: '',
+//             key: '',
+//             render: (_, record) => {
+//                 const { roles } = record
+//                 return (
+//                     <div>
+//                         <Dropdown
+//                             dropdownRender={(menus) => {
+//                                 return (
+//                                     <div className="shadow-lg rounded px-4 py-3 bg-white">
+//                                         {(roles || [])[0]?.name
+//                                             .toLowerCase()
+//                                             .includes('super') ? (
+//                                             <p className="max-w-[100px] font-medium text-[12px] text-lighterAsh">
+//                                                 This user is the super admin
+//                                             </p>
+//                                         ) : (
+//                                             <div className="flex flex-col">
+//                                                 <SettingActions
+//                                                     text="Resend Invite"
+//                                                     onClick={() =>
+//                                                         onHandleResendInvite({
+//                                                             email: record?.email,
+//                                                         })
+//                                                     }
+//                                                 />
+//                                                 <SettingActions text="Send Password Reset" />
+//                                                 <SettingActions
+//                                                     text="Remove User"
+//                                                     color="text-secondaryColor"
+//                                                 />
+//                                             </div>
+//                                         )}
+//                                     </div>
+//                                 )
+//                             }}
+//                             arrow={false}
+//                             trigger="click"
+//                         >
+//                             <div className="cursor-pointer">
+//                                 <SlOptionsVertical color="#67777E" />
+//                             </div>
+//                         </Dropdown>
+//                     </div>
+//                 )
+//             },
+//         },
+//     ]
+//     return (
+//         <div>
+//             <>
+//                 <div className="wallet-table mt-8">
+//                     <CustomTable
+//                         tableBorder={'none'}
+//                         filterBorder={`1px solid #EEEEEE`}
+//                         columns={columns}
+//                         tableName={'Filter Users'}
+//                         data={admins}
+//                         filterHeader
+//                         isLoading={loading}
+//                         showDateFilter={false}
+//                         showExportCSV={false}
+//                         handleRoleChange={onHandleRoleChange}
+//                         handleStatusChange={onHandleStatusChange}
+//                         //  rowClassName={'border border-red-900'}
+//                         pagination={{
+//                             hideOnSinglePage: true,
+//                             pageSize: 7,
+//                         }}
+//                     />
+//                 </div>
+//                 <CustomButton
+//                     text={'Invite New User'}
+//                     width="180px"
+//                     color={color.mainColor}
+//                     bg={color.fieldColor}
+//                     onClick={onAddUser}
+//                     className="mt-6"
+//                 />
+//             </>
+//         </div>
+//     )
+// }
+
+// export default UserManagement
--- src/services/slices/ledger/blocksTrx.js
+++ src/services/slices/ledger/blocksTrx.js
@@ -1,6 +1,29 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+// import { createAsyncThunk } from '@reduxjs/toolkit'
+// import { makeApiRequest } from '../../baseApi'
+// import { getFireBlockTrx } from '../../apis'
+
+// export const queryFireBlockTrx = createAsyncThunk(
+//     'fireblocktrx/queryFireBlockTrx',
+//     async ({ currency }) => {
+//         try {
+//             const response = await makeApiRequest(
+//                 'get',
+//                 getFireBlockTrx(currency)
+//             )
+//             return response?.data
+//         } catch (e) {}
+//     }
+// )
+
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { makeApiRequest } from '../../baseApi'
 import { getFireBlockTrx } from '../../apis'
+
+const initialState = {
+    loading: false,
+    error: null,
+    fireblockTrx: null,
+}
 
 export const queryFireBlockTrx = createAsyncThunk(
     'fireblocktrx/queryFireBlockTrx',
@@ -14,3 +37,29 @@ export const queryFireBlockTrx = createAsyncThunk(
         } catch (e) {}
     }
 )
+
+export const fireBlockTrxSlice = createSlice({
+    name: 'fireBlockTrx',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(queryFireBlockTrx.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(queryFireBlockTrx.fulfilled, (state, action) => {
+                state.loading = false
+                state.fireblockTrx = action?.payload?.data
+            })
+            .addCase(queryFireBlockTrx.rejected, (state, { payload }) => {
+                state.loading = false
+                state.error = payload
+            })
+    },
+})
+
+// A selector
+export const getFireblockTrxSelector = (state) => state.fireblockTrx
+
+// The reducer
+export default fireBlockTrxSlice.reducer
