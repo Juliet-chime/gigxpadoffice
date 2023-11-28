0d8a4b8a43ac5a906782f4089dc9d2826f32887b
chimeSomto <110493530+chimejuliet@users.noreply.github.com>
2023-11-28 14:21:17 +0100

    invite user

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -6,10 +6,14 @@ import AddAUser from './AddAUser'
 import RolesPermission from './RolesPermission'
 import GlobalConfiguration from './GlobalConfiguration/GlobalConfiguration'
 import AddARole from './AddARole'
+import { useErrorTimeout } from '../../hooks/useTimeout'
+import Notification from '../../components/notification/Notification'
 
 const Settings = () => {
     const [addUser, setAddUser] = useState(false)
     const [addRole, setAddRole] = useState(false)
+
+    const [message, setMessage, status, setStatus] = useErrorTimeout(3000)
 
     const onChangeTab = (key) => {
         alert(key)
@@ -19,7 +23,13 @@ const Settings = () => {
         {
             key: '1',
             label: `User Management`,
-            children: <UserManagement setAddUser={setAddUser} />,
+            children: (
+                <UserManagement
+                    setAddUser={setAddUser}
+                    setStatus={setStatus}
+                    setMessage={setMessage}
+                />
+            ),
         },
         {
             key: '2',
@@ -35,6 +45,18 @@ const Settings = () => {
 
     return (
         <div>
+            {!!message ? (
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '0px',
+                        left: '0px',
+                        right: '0px',
+                    }}
+                >
+                    <Notification message={message} type={status} />
+                </div>
+            ) : null}
             {addUser ? (
                 <AddAUser setAddUser={setAddUser} />
             ) : addRole ? (
@@ -62,3 +84,68 @@ const Settings = () => {
 }
 
 export default Settings
+
+// import React, { useState } from 'react'
+// import Dashboardheader from '../../components/dashboardComponents/Dashboardheader'
+// import { Tabs } from 'antd'
+// import UserManagement from './UserManagement'
+// import AddAUser from './AddAUser'
+// import RolesPermission from './RolesPermission'
+// import GlobalConfiguration from './GlobalConfiguration/GlobalConfiguration'
+// import AddARole from './AddARole'
+
+// const Settings = () => {
+//     const [addUser, setAddUser] = useState(false)
+//     const [addRole, setAddRole] = useState(false)
+
+//     const onChangeTab = (key) => {
+//         alert(key)
+//     }
+
+//     const items = [
+//         {
+//             key: '1',
+//             label: `User Management`,
+//             children: <UserManagement setAddUser={setAddUser} />,
+//         },
+//         {
+//             key: '2',
+//             label: `Roles and Permissions`,
+//             children: <RolesPermission setAddRole={setAddRole} />,
+//         },
+//         {
+//             key: '3',
+//             label: `Global Configuration`,
+//             children: <GlobalConfiguration />,
+//         },
+//     ]
+
+//     return (
+//         <div>
+//             {addUser ? (
+//                 <AddAUser setAddUser={setAddUser} />
+//             ) : addRole ? (
+//                 <AddARole setAddRole={setAddRole} />
+//             ) : (
+//                 <>
+//                     <Dashboardheader
+//                         componentName={'System Settings'}
+//                         label={
+//                             'Manage and control all system and dashboard settings'
+//                         }
+//                         className="mb-6"
+//                     />
+//                     <div className="detailcard mt-12">
+//                         <Tabs
+//                             defaultActiveKey="1"
+//                             items={items}
+//                             onChange={onChangeTab}
+//                         />
+//                     </div>
+//                 </>
+//             )}
+//         </div>
+//     )
+// }
+
+// export default Settings
