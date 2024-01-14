589f2ba7ae0eb574580db59184a6f7edfc4d4f70
echilaka1 <emmanuelchilaka779@gmail.com>
2024-01-14 11:56:54 +0100

    updated admin detail drawer

--- src/components/table/StatusTag.jsx
+++ src/components/table/StatusTag.jsx
@@ -5,9 +5,9 @@ const StatusTag = ({ text, ...props }) => {
         <h1
             className={`${text === 'success' ||
                 text === 'successful' ||
-                text === 'Completed' || text === 'completed'
+                text === 'Completed' || text === 'completed' || text === 'active'
                 ? 'bg-[#E8FFF7]'
-                : text === 'failed'
+                : text === 'failed' || text === 'inactive'
                     ? 'bg-[#ff14141a]'
                     : text === 'pending'
                         ? 'bg-[#ea7d1f1a]'
@@ -16,7 +16,7 @@ const StatusTag = ({ text, ...props }) => {
                     text === 'successful' ||
                     text === 'Completed' || text === 'completed'
                     ? 'text-[#27B785]'
-                    : text === 'failed'
+                    : text === 'failed' || text === 'inactive'
                         ? 'text-[#FF1414]'
                         : text === 'pending'
                             ? 'text-[#EA7D1F]'
