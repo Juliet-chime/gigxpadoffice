ca8430ba3f96301085d15d2ece9684d11da67cd9
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2024-01-24 17:46:13 +0300

    vertical align fix

--- src/components/layout/NonAuthLayout.js
+++ src/components/layout/NonAuthLayout.js
@@ -22,7 +22,7 @@ const NonAuthLayout = ({
 }) => {
     return (
         <LayoutContainer className="h-full main-layout-div">
-            <div>
+            <div className="vertically-align">
                 <Row>
                     {/* <Col xs={0} sm={0} md={0} lg={8} xl={8}>
                         <LayoutBgContainer image={image}>
